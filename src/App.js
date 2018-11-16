b2a379b5a698b04a0fddbb746d768cee2ead3df3
Robin Cai <zhicaidev@gmail.com>
2018-11-17 00:38:24 +1100

    add component properties

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working! </p>
-                <Person />
-                <Person />
-                <Person />
+                <Person name="max" age="28" />
+                <Person name="Manu" age="28"> My Hobbies: Racing</Person>
+                <Person name="Stephanie" age="27" />
             </div>
         );
         // Equal to below
