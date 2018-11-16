d9e2fa4cda7b2e6c15a02de81411114be779c5c2
Robin Cai <zhicaidev@gmail.com>
2018-11-17 00:49:38 +1100

    38 - add state in component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: 'Max', age: 28},
+            {name: 'Manu', age: 29},
+            {name: 'Stephanie', age: 26}
+        ]
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working! </p>
-                <Person name="max" age="28" />
-                <Person name="Manu" age="28"> My Hobbies: Racing</Person>
-                <Person name="Stephanie" age="27" />
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My Hobbies: Racing</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
             </div>
         );
         // Equal to below
